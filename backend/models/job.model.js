556ae57a90a35b41bcfc3b14812bb9faec499977
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-06-12 18:30:43 +0530

    added job model

---
+++ backend/models/job.model.js
@@ -0,0 +1,47 @@
+import mongoose, { mongo } from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    requirements: {
+        type: String,
+        // required: true,
+    },
+    salary: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    openings: {
+        type: Number,
+        required: true,
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true,
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    applications: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application',
+            // required: true,
+        }
+    ]
+})
+
+export const Job= mongoose.model('Job', jobSchema);
