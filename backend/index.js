5414749f8e6c8748e95363a39f5b60429f9e10a9
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-06-20 00:37:41 +0530

    add application routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import connectDB from "./utils/db.js";
 import userRoutes from "./routes/user.route.js";
 import companyRoutes from"./routes/company.route.js";
 import jobRoutes from "./routes/job.route.js";
+import applicationRoutes from "./routes/application.route.js";
 import "./models/application.model.js";
 dotenv.config({});
 
@@ -25,10 +26,11 @@ app.use(cors(corsOptions));
 
 const PORT= process.env.PORT || 3000;
 
-//APIs
+//API Routes
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/companies", companyRoutes);
 app.use("/api/v1/jobs", jobRoutes);
+app.use("/api/v1/applications", applicationRoutes);
 
 
 app.listen(PORT, ()=>{
---
+++ backend/routes/application.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import isLoggedIn from "../middlewares/isLoggedIn.js";
+import { applyJob, getApplicants, getAppliedJobs, updateJobStatus } from "../controllers/application.controller.js";
+
+const router = express.Router();
+
+router.route("/:id").post(isLoggedIn, applyJob)  // Apply for a new job
+router.route("/").get(isLoggedIn, getAppliedJobs);     // Get all applied jobs
+router.route("/:id/applicants").get(isLoggedIn, getApplicants);   // Get all applicants
+router.route("/:id/status").patch(isLoggedIn, updateJobStatus);  // Update job status
+
+//using verbs like "update" in the API path is not recommended in RESTful APIs pattern, rather nouns/ resources' name shd be used
+//verbs are already indicated in HTTP method names
+
+export default router;
+
