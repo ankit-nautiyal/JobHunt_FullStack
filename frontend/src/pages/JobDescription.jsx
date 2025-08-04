7cc82127065b82530871263bbf44eb17ae51c350
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-08-05 00:42:11 +0530

    add applyJob functionality

--- frontend/src/pages/JobDescription.jsx
+++ frontend/src/pages/JobDescription.jsx
@@ -1,34 +1,54 @@
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { setSingleJob } from '@/redux/jobSlice';
-import { JOB_API_ENDPOINT } from '@/utils/constants';
+import { APPLICATION_API_ENDPOINT, JOB_API_ENDPOINT } from '@/utils/constants';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
 
 
 const JobDescription = () => {
-    const dispatch= useDispatch();
-    const {singleJob}= useSelector(store => store.job);
-    const {user}= useSelector(store => store.auth);
+    const dispatch = useDispatch();
+    const { singleJob } = useSelector(store => store.job);
+    const { user } = useSelector(store => store.auth);
 
-    const isApplied = singleJob?.applications?.some(application => application.applicant === user._id) || false; 
+    const isIntiallyApplied = singleJob?.applications?.some(application => application.applicant === user?._id) || false;
+    const [isApplied, setIsApplied] = useState(isIntiallyApplied);
+
     //redundant to write 'true' using ternary optr as given below, as some() fn always returns a boolean value
     // const isApplied = singleJob?.applications?.some(application => application.applicant === user._id) ? true : false;
 
     const params = useParams();  //to fetch params from URL
     const jobId = params.id;   //to fetch :id from params
 
+    const handleApplyJob = async () => {
+        try {
+            //axios.post(url, data/payload, config);
+            const res = await axios.post(`${APPLICATION_API_ENDPOINT}/${jobId}`, {}, { withCredentials: true });
+
+            if (res.data.success) {
+                setIsApplied(true);  //Updates the local state for UI update
+                const updatedSingleJob = { ...singleJob, applications: [...singleJob.applications, { applicant: user?._id }] }
+                dispatch(setSingleJob(updatedSingleJob));  //for real time UI update (updates total applciants)
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        }
+    }
+
     useEffect(() => {
         const fetchSingleJob = async () => {
             try {
                 //axios.get(url, config);
                 const res = await axios.get(`${JOB_API_ENDPOINT}/${jobId}`, { withCredentials: true });
-                console.log(res);
-                
+
                 if (res.data.success) {
                     dispatch(setSingleJob(res.data.job));
+                    setIsApplied(res.data.job.applications.some(application=>application.applicant === user?._id)) //Ensure the state is in sync with fetched data
                 }
             } catch (error) {
                 console.log('Error fetching jobs:', error);
@@ -49,6 +69,7 @@ const JobDescription = () => {
                     </div>
                 </div>
                 <Button
+                    onClick={isApplied ? null : handleApplyJob}
                     disabled={isApplied}
                     className={`rounded-lg ${isApplied ? 'cursor-not-allowed' : 'bg-[#7e5db7] cursor-pointer hover:bg-[#9983bc]'}`}
                 >
