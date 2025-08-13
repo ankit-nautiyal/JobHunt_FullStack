7e56f1fe1b55707bb8e4f7e75b4d4eae6803c547
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-08-14 02:17:33 +0530

    update company register route

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -4,7 +4,7 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
-router.route("/register").post(isAuthenticated, registerCompany)  // Register a new company
+router.route("/").post(isAuthenticated, registerCompany)  // Register a new company
 router.route("/").get(isAuthenticated, getCompanies);      // Get all companies
 router.route("/:id").get(isAuthenticated, getCompanyById)   // Get company by ID
                     .patch(isAuthenticated, updateCompany);  // Update company by ID
