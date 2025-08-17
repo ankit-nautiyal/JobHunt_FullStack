825c1531948c3efd1ad294988635a239a01c8cc4
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-08-18 02:51:45 +0530

    update application route

--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route("/:id").post(isAuthenticated, applyJob)  // Apply for a new job
 router.route("/").get(isAuthenticated, getAppliedJobs);     // Get all applied jobs
 router.route("/:id/applicants").get(isAuthenticated, getApplicants);   // Get all applicants of a job
-router.route("/:id/status").patch(isAuthenticated, updateJobStatus);  // Update job status
+router.route("/:id/status").patch(isAuthenticated, updateJobStatus);  // Update application status
 
 //using verbs like "update" in the API path is not recommended in RESTful APIs pattern, rather nouns/ resources' name shd be used
 //verbs are already indicated in HTTP method names
