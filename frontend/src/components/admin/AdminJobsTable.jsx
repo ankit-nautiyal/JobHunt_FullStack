cbcc60cada7b3e83cd2bfc89177751fbd76f79a2
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-08-19 20:10:09 +0530

    update AdminJobsTable

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -55,10 +55,10 @@ const AdminJobsTable = () => {
                                     <Popover>
                                         <PopoverTrigger className='cursor-pointer'> <MoreHorizontal /></PopoverTrigger>
                                         <PopoverContent className='w-32'>
-                                            <div onClick={() => navigate(`/admin/jobs/${job._id}`)} className='flex items-center gap-2 w-fit cursor-pointer'>
+                                            {/* <div onClick={() => navigate(`/admin/jobs/${job._id}`)} className='flex items-center gap-2 w-fit cursor-pointer'>
                                                 <Edit2 className='w-4' />
                                                 <span>Edit</span>
-                                            </div>
+                                            </div> */}
                                             <div onClick={() => navigate(`/admin/jobs/${job._id}/applicants`)} className='flex items-center gap-2 w-fit cursor-pointer'>
                                                 <Eye className='w-4' />
                                                 <span>Applicants</span>
