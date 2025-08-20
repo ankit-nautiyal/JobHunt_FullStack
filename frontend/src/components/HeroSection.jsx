ef86c07ef234d00662c649eb16bb865a921069f3
Ankit Nautiyal <nautiyalankit65@gmail.com>
2025-08-20 19:25:16 +0530

    update HeroSection

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -20,7 +20,7 @@ const HeroSection = () => {
             <div className=' flex flex-col gap-5 my-10'>
                 <span className=' mx-auto bg-gray-100 rounded-full px-4 py-2 font-medium text-[#F83002]'>No. #1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'> Dream Jobs!</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem laboriosam itaque porro fugiat non.</p>
+                <p>JobHunt is India's No. #1 Job Portal for finding latest job & internship opprtunites in startups as well as product based firms!</p>
 
                 <div className='flex items-center w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full gap-4 mx-auto my-5'>
                     <input
